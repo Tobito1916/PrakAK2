b22e58a036c9879224e0c060995718b9b1e4f096
Tobito1916 <jonas.hoesl@gmx.de>
2018-05-23 17:35:05 +0200

    Read in hinzugefügt

---
+++ src/Read.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Read {
+   static String readin() throws IOException {
+       String text = "";
+       try {
+           BufferedReader br = new BufferedReader(new FileReader("src/chiffrat.txt"));
+           while (br.ready()) { // while loop begins here
+               text = text + br.readLine();
+
+           }
+
+       } catch (IOException e) {
+           System.out.println("Error");//f
+       }
+       return text;
+   }
+
+
+}
